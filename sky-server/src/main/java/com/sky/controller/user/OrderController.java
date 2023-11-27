1caf852b647b993d9bcd871d4d2a0c3fb27ec2e9
kendrick <1361855095@qq.com>
2023-11-27 17:51:21 +0800

    客户催单业务代码开发

--- sky-server/src/main/java/com/sky/controller/user/OrderController.java
+++ sky-server/src/main/java/com/sky/controller/user/OrderController.java
@@ -109,4 +109,16 @@ public class OrderController {
         orderService.repeatition(id);
         return Result.success();
     }
+
+    /**
+     * 催单
+     * @param id
+     * @return
+     */
+    @GetMapping("/reminder/{id}")
+    @ApiOperation("催单")
+    public Result reminder(@PathVariable("id") Long id){
+        orderService.reminder(id);
+        return Result.success();
+    }
 }
